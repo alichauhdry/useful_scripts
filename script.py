69ec8542043dd70abdb0b9cd640a4c3a1db71133
alichauhdry <47738075+alichauhdry@users.noreply.github.com>
2019-04-10 10:22:33 +0200

    Added some user friendliness

--- script.py
+++ script.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import fileinput
 import re
 import sys
@@ -8,6 +10,8 @@ log_file_path = sys.argv[1]
 #regex = '[0-9]+\:[0-9]+\:[0-9]+(\.|\:)[0-9]+' #For time stamps not for dates
 #regex = '0x[0-9]{0,3}([a-z]|[A-Z]|[0-9]){0,4}' #For memory addresses
 #regex = '\s[0-9].[0-9]{6}\sms' #For timespan
+
+print ("Ready to remove extra stuff in file: %s" % log_file_path)
 
 regex = '((=\s[0-9]{0,3}.[0-9]{6,10}\sms)|(0x[0-9]{0,3}([a-z]|[A-Z]|[0-9]){0,4})|([0-9]+\:[0-9]+\:[0-9]+(\.|\:)[0-9]+))'
 
@@ -22,13 +26,15 @@ for line in log_file:
  file_buffer.append(re.sub(pattern,'',line))
  if (pattern.search(line) != None):
   flag = flag + 1
-  if(flag%100 == 0):
-   print("+"),
 
-new_log_file = open (log_file_path+"_parsed", 'w')
+new_log_file_name = log_file_path+"_parsed"
+
+new_log_file = open (new_log_file_name, 'w')
 
 new_log_file.writelines(file_buffer)
 
 log_file.close()
 new_log_file.close()
+
+print ("Total alterations have been done under file name: '%s' are: %d" % (new_log_file_name,flag))
 print ("DONE")
